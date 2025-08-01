323af85a93d0173aeb4cdec2e7297bacec7080ba
seulgeh <kangseulgi910@gmail.com>
2025-08-01 16:46:44 +0700

    Update vite.config.js

--- gfday-flower-gift/vite.config.js
+++ gfday-flower-gift/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- tambahkan ini
+  base: '/bunga-flower/', // <-- HARUS absolute path, bukan './'
 })
